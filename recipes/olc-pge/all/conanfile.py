f5c6178b46d499fb7691d1a11fbb65c8070b2ba0
Rene Rivera <grafikrobot@gmail.com>
2021-09-10 23:38:33 -0500

    Avoid upload attempts and usage.

--- recipes/olc-pge/all/conanfile.py
+++ recipes/olc-pge/all/conanfile.py
@@ -18,7 +18,7 @@ class Package(ConanFile):
     description = "The olcPixelGameEngine is a single-file prototyping and game-engine framework created in C++."
     topics = (
         "olc", "pge", "pixelengine", "pixelgameengine", "pgex",
-        "engine", "graphics",
+        "engine", "graphics", "opengl",
         "game-engine", "game-development", "game-dev", "gamedev", "games")
     license = "OLC-3"
     url = "https://github.com/bfgroup/conan/tree/main/recipes/olc-pge"
